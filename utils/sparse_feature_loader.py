2744f88832e9b9e7a5bbb7a8e0a5b1bc810b9db9
Constantine Belev <const.belev@yandex.com>
2018-05-22 13:39:53 +0300

    get code refactored a bit, improved disk reading pattern by building submatrix from sorted indices then sorting back in-memory, added optional tqdm progress bar for submatrix generation

--- utils/sparse_feature_loader.py
+++ utils/sparse_feature_loader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import tqdm
 import pickle
 
 from scipy.sparse import csr_matrix
@@ -47,33 +48,40 @@ class SparseFeatureLoader:
         with open(os.path.join(data_path, 'column_names.pkl'), 'rb') as handle:
             self.columns = pickle.load(handle)
 
-    def _get(self, indices_to_get):
+    def _get(self, indices_to_get, verbose=0):
+        if not self._memmap:
+            return self.data[indices_to_get]
+
         if np.isscalar(indices_to_get):
             indices_to_get = [indices_to_get]
-        indices_to_get = np.array(indices_to_get, dtype=int)
+        indptr = np.array(self.indptr_memmap)
 
-        indices_delta = np.zeros(len(indices_to_get) + 1, dtype=self.indptr_memmap.dtype)
-        indices_delta[1:] = self.indptr_memmap[indices_to_get + 1] - self.indptr_memmap[indices_to_get]
+        indices_to_get = np.array(indices_to_get, dtype=int)
+        indices_to_get_argsort = indices_to_get.argsort()
+        indices_to_get_aasort = indices_to_get_argsort.argsort()
+        indices_to_get_sorted = indices_to_get[indices_to_get_argsort]
+
+        indices_delta = np.zeros(len(indices_to_get_sorted) + 1, dtype=self.indptr_memmap.dtype)
+        indices_delta[1:] = indptr[indices_to_get_sorted + 1] - indptr[indices_to_get_sorted]
 
         new_indptr = np.cumsum(indices_delta)
         new_data = np.zeros(new_indptr[-1], dtype=self.data_memmap.dtype)
         new_col_idx = np.zeros(new_indptr[-1], dtype=self.col_idx_memmap.dtype)
 
-        for new_index, index_to_get in enumerate(indices_to_get):
+        for new_index, index_to_get in tqdm.tqdm_notebook(enumerate(indices_to_get_sorted),
+                                                          total=len(indices_to_get_sorted), disable=(verbose == 0)):
             new_data[new_indptr[new_index]: new_indptr[new_index + 1]] = \
-                self.data_memmap[self.indptr_memmap[index_to_get]:self.indptr_memmap[index_to_get + 1]]
+                self.data_memmap[indptr[index_to_get]:indptr[index_to_get + 1]]
             new_col_idx[new_indptr[new_index]: new_indptr[new_index + 1]] = \
-                self.col_idx_memmap[self.indptr_memmap[index_to_get]:self.indptr_memmap[index_to_get + 1]]
+                self.col_idx_memmap[indptr[index_to_get]:indptr[index_to_get + 1]]
 
-        return csr_matrix((new_data, new_col_idx, new_indptr),
-                          shape=(len(indices_to_get), self.sparse_matrix_info['shape'][1]), dtype=new_data.dtype)
+        mat = csr_matrix((new_data, new_col_idx, new_indptr),
+                         shape=(len(indices_to_get), self.sparse_matrix_info['shape'][1]), dtype=new_data.dtype)
+        return mat[indices_to_get_aasort]
 
-    def get(self, operation_ids):
+    def get(self, operation_ids, verbose=0):
         operation_indices = [self.operationid_counter[op] for op in operation_ids if op in self.operationid_counter]
-        if self._memmap:
-            return self._get(operation_indices)
-        else:
-            return self.data[operation_indices]
+        return self._get(operation_indices, verbose=verbose)
 
     def have_features_for_ids(self, operation_ids):
         return np.array([op in self.operationid_counter for op in operation_ids])
